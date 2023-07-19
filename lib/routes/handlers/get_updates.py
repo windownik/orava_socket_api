313ad81c31836e0d8752915d88887423b524d17b
windownik <windownik@gmail.com>
2023-07-19 15:41:01 +0300

    add new socket handler get_updates

--- lib/routes/handlers/get_updates.py
+++ lib/routes/handlers/get_updates.py
@@ -24,6 +24,7 @@ async def handler_get_updates(msg: dict, db: Depends, websocket: WebSocket):
         for one in messages:
 
             new_msg = Message.parse_obj(one)
+            print(new_msg)
             all_message.append(new_msg.dict())
 
     print(len(all_message), all_message)

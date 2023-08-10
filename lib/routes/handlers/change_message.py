074d47493a1e8ef96109e01c24046e8547bc2d4b
windownik <windownik@gmail.com>
2023-08-10 11:24:15 +0300

    fix bags

--- lib/routes/handlers/change_message.py
+++ lib/routes/handlers/change_message.py
@@ -9,22 +9,22 @@ from lib.routes.socket_resp import SocketRespGetUpdates
 
 async def handler_change_msg(msg: dict, db: Depends, websocket: WebSocket, manager: ConnectionManager,
                              reqwest_user: User):
-    change_msg = ChangeMsg.parse_obj(msg)
+    change_msg = Message.parse_obj(msg['message'])
     socket_resp = SocketRespGetUpdates()
 
     # Проверяем права доступа на сообщение
-    owner_id = await conn.get_token(db=db, token_type='refresh', token=change_msg.refresh_token)
+    owner_id = await conn.get_token(db=db, token_type='refresh', token=msg['refresh_token'])
     if not owner_id:
         await websocket.send_json(socket_resp.response_401)
         return True
     print('check user')
     await conn.update_msg(db=db, msg=change_msg)
     print("update message")
-    msg_data = await conn.read_data(table='messages', id_name='msg_id', id_data=change_msg.message.msg_id, db=db)
-    print(change_msg.message.msg_id, msg_data)
+    msg_data = await conn.read_data(table='messages', id_name='msg_id', id_data=change_msg.msg_id, db=db)
+    print(change_msg.msg_id, msg_data)
     new_msg = Message.parse_obj(msg_data[0])
     msg_json = await add_user_and_reply_to_msg(db=db, msg=new_msg, reqwest_user=reqwest_user)
-    all_users = await conn.read_data(table='users_chat', id_name='chat_id', id_data=change_msg.message.chat_id, db=db)
+    all_users = await conn.read_data(table='users_chat', id_name='chat_id', id_data=change_msg.chat_id, db=db)
 
     await manager.broadcast_all_in_chat(body={
         "status_code": 200,
